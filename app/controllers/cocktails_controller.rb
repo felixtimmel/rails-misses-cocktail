6dcfe8fa848e781d987337e3646c83be40f95d34
Felix Timmel <f.timmel95@gmail.com>
2020-02-20 21:55:42 -0500

    make the website a little bit prettyer

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,39 @@
 class CocktailsController < ApplicationController
+  def index
+    if params[:search].nil?
+      @cocktails = Cocktail.all
+    else
+      @search = params[:search]
+      @cocktails = Cocktail.where("lower(#{:name}) LIKE ?", "%#{@search.downcase}%")
+    end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(safe_params)
+    if @cocktail.save
+      redirect_to cocktails_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def safe_params
+    params.require(:cocktail).permit(:name)
+  end
 end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(safe_params)
+    @dose.cocktail_id = params[:cocktail_id]
+    @dose.ingredient_id = params[:dose][:ingredient_id]
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail_id)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def safe_params
+    params.require(:dose).permit(:description, :ingredient)
+  end
+end
